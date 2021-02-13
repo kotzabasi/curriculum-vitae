bdaaee9dc8fb636301ffbf1fe29a6f5a2b0dee57
kotzabasi <kotzabasi@hotmail.com>
2021-02-13 18:40:26 +0200

    Initial commit

---
+++ README.txt
@@ -0,0 +1,11 @@
+CURRICULUM VITAE
+
+Every time I learned something new and I had to add it in my CV, I had to find a new template and make the CV all over again.
+When I finished a Bootstrap4 course I though to practice my new skill, developing a CV page.
+
+FEATURES:
+
+Bootstrap4, HTML5, CSS3, JQUERY & Animate.css Library
+
+Deployment:
+HEROKU
---
+++ script/myScript.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+  $("#back").click(function () {
+    parent.history.back();
+    return false;
+  });
+
+  $("#skillJavascript").addClass("animate__animated animate__backInLeft");
+  $("#skillJquery").addClass("animate__animated animate__backInRight");
+  $("#skillSql").addClass("animate__animated animate__backInLeft");
+  $("#skillJava").addClass("animate__animated animate__backInRight");
+  $("#skillCss").addClass("animate__animated animate__backInLeft");
+  $("#skillHtml").addClass("animate__animated  animate__backInRight");
+  $("#skillnode").addClass("animate__animated animate__backInLeft");
+  $("#skillMongo").addClass("animate__animated animate__backInRight");
+  $("#skillsass").addClass("animate__animated animate__backInLeft");
+  $("#skillFlexbox").addClass("animate__animated animate__backInRight");
+  $("#skillwordpress").addClass("animate__animated animate__backInLeft");
+  $("#skillElementor").addClass("animate__animated animate__backInRight");
+  $("#skillBootstrap").addClass("animate__animated animate__backInLeft");
+  $("#skillEcommerce").addClass("animate__animated animate__backInRight");
+
+
+
+
+});
